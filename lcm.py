a712e8df25dc6029c2d74ebd33a011be48487245
Kartik Arora <76392870+kartikarora1@users.noreply.github.com>
2020-12-25 18:20:01 +0530

    To find least common multiple

---
+++ lcm.py
@@ -0,0 +1,20 @@
+def lcm(x, y):
+
+   # choose the greater number
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
+
+num1 = int(input())
+num2 = int(input())
+
+print("The L.C.M. is", lcm(num1, num2))
