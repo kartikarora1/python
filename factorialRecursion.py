12e972ed08ad387d241c9f1081861085386353a0
Kartik Arora <76392870+kartikarora1@users.noreply.github.com>
2020-12-30 14:53:32 +0530

    Factorial of a number using recursion

---
+++ factorialRecursion.py
@@ -0,0 +1,13 @@
+def factorial(n):
+   if n == 1:
+       return n
+   else:
+       return n*factorial(n-1)
+
+num = int(input())
+if num < 0:
+   print("Factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   print(factorial(num))
