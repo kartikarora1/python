e2bca99110336f32048e48284bc1d77ea482698c
Kartik Arora <kartik1492.cse18@chitkara.edu.in>
2021-01-29 19:47:00 +0530

    Generating pattern from number provided by user

---
+++ patternmaking.py
@@ -0,0 +1,7 @@
+n=int(input("Enter number for pattern:-"))
+for i in range(n):
+    s="-".join(chr(ord('a')+n-j-1)for j in range(i+1))
+    print((s+s[::-1][1:]).center(n*4-3,'-'))
+for i in range(n-1):
+    s="-".join(chr(ord('a')+n-j-1)for j in range(n-i-1))
+    print((s+s[::-1][1:]).center(n*4-3,'-'))
