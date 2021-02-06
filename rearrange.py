7e49880690816b4c1c23e9581a09bb104070ab65
Kartik Arora <kartik1492.cse18@chitkara.edu.in>
2021-02-06 19:10:15 +0530

    To rearrange positive and negative numbers

---
+++ rearrange.py
@@ -0,0 +1,5 @@
+array_nums = [-1, 2, -3, 5, 7, 8, 9, -10]
+print(array_nums)
+result = sorted(array_nums, key = lambda i: 0 if i == 0 else -1 / i)
+print("\nRearrange positive and negative numbers: ")
+print(result)
